4e86cdce930669258898041aa186a5545762a167
daniel <danielbolster@protonmail.com>
2021-09-14 21:05:15 -0400

    working tmux bindings

--- main.ts
+++ main.ts
@@ -8,22 +8,40 @@ import
 	Setting
 } from 'obsidian';
 
+
 export default class MyPlugin extends Plugin
 {
 	async onload()
 	{
 		console.log('loading plugin');
 
-
+		var prefix : boolean;
 		this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
 			if(evt.key == 'b' && evt.ctrlKey)
 			{
-				alert("prefix")
-				//console.log(this.app as any).commands.executeCommandById('')
-				this.app.commands.executeCommandById("workspace:split-vertical")
-			}
+				prefix = true
 
+			}
+			else if(evt.key == '%' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("workspace:split-vertical")
+				prefix = false
+			}
+			else if(evt.key == '"' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("workspace:split-horizontal")
+				prefix = false
+			}
+			else if(evt.key == 'w' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("workspace:close")
+				prefix = false
+			}
 		});
+
 	}
 
 }
