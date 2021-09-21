8d2d28d86896ed30319ffd5177560d30df8b31e6
Versari <danielbolster@protonmail.com>
2021-09-21 00:21:51 -0400

    added arrow key/vim key support

--- main.ts
+++ main.ts
@@ -40,6 +40,46 @@ export default class MyPlugin extends Plugin
 				application.commands.executeCommandById("workspace:close")
 				prefix = false
 			}
+			else if(evt.key == 'ArrowLeft' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-left")
+			}
+			else if(evt.key == 'ArrowRight' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-right")
+			}
+			else if(evt.key == 'ArrowUp' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-top")
+			}
+			else if(evt.key == 'ArrowDown' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-bottom")
+			}
+			else if(evt.key == 'h' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-left")
+			}
+			else if(evt.key == 'l' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-right")
+			}
+			else if(evt.key == 'k' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-top")
+			}
+			else if(evt.key == 'j' && prefix == true)
+			{
+				var application : any = this.app
+				application.commands.executeCommandById("editor:focus-bottom")
+			}
 		});
 
 	}
